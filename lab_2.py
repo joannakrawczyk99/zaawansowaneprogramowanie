97ebe5358fb0b046755df51c6a8f52727783aefa
Joanna <joannakrawczyk99@gmail.com>
2021-10-19 19:39:44 +0200

    with return statement

--- lab_2.py
+++ lab_2.py
@@ -1,37 +1,45 @@
 def task_a(names):
-    for i in range(len(names)):
-        print(names[i])
-    print()
+    return names
 
 def task_b(int_list):
-    for i in range(len(int_list)):
-        print(int_list[i]*2)
-    print()
+    #new_list =[]
+    #for i in range(len(int_list)):
+        #new_list.append(int_list[i] * 2)
+    #return new_list
 
     new_list = [i*2 for i in int_list]
-    print(new_list)
-    print()
+    return(new_list)
 
 def task_c(list_of_ten_ints):
+    new_list = []
     for i in range(len(list_of_ten_ints)):
         if list_of_ten_ints[i] % 2 == 0:
-            print(list_of_ten_ints[i])
-    print()
+            new_list.append(list_of_ten_ints[i])
+    return new_list
 
 def task_d(list_of_ten_ints):
+    new_list = []
     for i in range(len(list_of_ten_ints)):
         if i%2==0:
-            print(list_of_ten_ints[i])
+            new_list.append(list_of_ten_ints[i])
+    return new_list
 
 
 if __name__ == '__main__':
     names = ['Asia', 'Ania', 'Jacek', 'Tomek', 'Paweł']
-    task_a(names)
+    a = task_a(names)
+    print(a)
+    print()
 
     list_of_int = [1,2,3,4,5]
-    task_b(list_of_int)
+    b = task_b(list_of_int)
+    print(b)
+    print()
 
     list_of_ten_ints = [2,2,2,4,5,6,7,8,9,10]
-    task_c(list_of_ten_ints)
-    task_d(list_of_ten_ints)
-
+    c = task_c(list_of_ten_ints)
+    print(c)
+    print()
+    
+    d = task_d(list_of_ten_ints)
+    print(d)
