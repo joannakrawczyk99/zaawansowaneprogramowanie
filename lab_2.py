0934ab777f5d4a4eeedbc53c483def7ff05d40d5
Joanna <joannakrawczyk99@gmail.com>
2021-11-03 18:22:35 +0100

    with PEP8 standard

--- lab_2.py
+++ lab_2.py
@@ -1,14 +1,16 @@
 def task_a(names):
     return names
 
-def task_b(int_list):
-    #new_list =[]
-    #for i in range(len(int_list)):
-        #new_list.append(int_list[i] * 2)
-    #return new_list
 
-    new_list = [i*2 for i in int_list]
-    return(new_list)
+def task_b(int_list):
+    # new_list =[]
+    # for i in range(len(int_list)):
+    # new_list.append(int_list[i] * 2)
+    # return new_list
+
+    new_list = [i * 2 for i in int_list]
+    return (new_list)
+
 
 def task_c(list_of_ten_ints):
     new_list = []
@@ -17,10 +19,11 @@ def task_c(list_of_ten_ints):
             new_list.append(list_of_ten_ints[i])
     return new_list
 
+
 def task_d(list_of_ten_ints):
     new_list = []
     for i in range(len(list_of_ten_ints)):
-        if i%2==0:
+        if i % 2 == 0:
             new_list.append(list_of_ten_ints[i])
     return new_list
 
@@ -31,15 +34,15 @@ if __name__ == '__main__':
     print(a)
     print()
 
-    list_of_int = [1,2,3,4,5]
+    list_of_int = [1, 2, 3, 4, 5]
     b = task_b(list_of_int)
     print(b)
     print()
 
-    list_of_ten_ints = [2,2,2,4,5,6,7,8,9,10]
+    list_of_ten_ints = [2, 2, 2, 4, 5, 6, 7, 8, 9, 10]
     c = task_c(list_of_ten_ints)
     print(c)
     print()
-    
+
     d = task_d(list_of_ten_ints)
     print(d)
