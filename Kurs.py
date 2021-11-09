89adb9b279a601d6e99fcad0055dcb4709f11fbc
Joanna <joannakrawczyk99@gmail.com>
2021-11-09 15:00:45 +0100

    kolokwium 1

--- Kurs.py
+++ Kurs.py
@@ -12,45 +12,45 @@ class Kurs:
         self.marka_pojazdu = marka_pojazdu
         self.idn_kierowcy = idn_kierowcy
 
-        @property
-        def miejscowosc_poczatkowa(self) -> None:
-            return self.miejscowosc_poczatkowa
+    @property
+    def miejscowosc_poczatkowa(self) -> None:
+        return self.miejscowosc_poczatkowa
 
-        @property
-        def miejscowosc_docelowa(self) -> None:
-            return self.miejscowosc_docelowa
+    @property
+    def miejscowosc_docelowa(self) -> None:
+        return self.miejscowosc_docelowa
 
-        @property
-        def odleglosc(self) -> None:
-            return self.odleglosc
+    @property
+    def odleglosc(self) -> None:
+        return self.odleglosc
 
-        @property
-        def marka_pojazdu(self) -> None:
-            return self.marka_pojazdu
+    @property
+    def marka_pojazdu(self) -> None:
+        return self.marka_pojazdu
 
-        @property
-        def idn_kierowcy(self) -> None:
-            return self.idn_kierowcy
+    @property
+    def idn_kierowcy(self) -> None:
+        return self.idn_kierowcy
 
-        @miejscowosc_poczatkowa.setter
-        def miejscowosc_poczatkowa(self, miejscowosc_poczatkowa: str) -> None:
-            self.miejscowosc_poczatkowa = miejscowosc_poczatkowa
+    @miejscowosc_poczatkowa.setter
+    def miejscowosc_poczatkowa(self, miejscowosc_poczatkowa: str) -> None:
+        self.miejscowosc_poczatkowa = miejscowosc_poczatkowa
 
-        @miejscowosc_docelowa.setter
-        def miejscowosc_docelowa(self, miejscowosc_docelowa: str) -> None:
-            self.miejscowosc_docelowa = miejscowosc_docelowa
+    @miejscowosc_docelowa.setter
+    def miejscowosc_docelowa(self, miejscowosc_docelowa: str) -> None:
+        self.miejscowosc_docelowa = miejscowosc_docelowa
 
-        @odleglosc.setter
-        def odleglosc(self, odleglosc: list) -> None:
-            self.odleglosc = odleglosc
+    @odleglosc.setter
+    def odleglosc(self, odleglosc: list) -> None:
+        self.odleglosc = odleglosc
 
-        @marka_pojazdu.setter
-        def marka_pojazdu(self, marka_pojazdu: str) -> None:
-            self.marka_pojazdu = marka_pojazdu
+    @marka_pojazdu.setter
+    def marka_pojazdu(self, marka_pojazdu: str) -> None:
+        self.marka_pojazdu = marka_pojazdu
 
-        @idn_kierowcy.setter
-        def idn_kierowcy(self, idn_kierowcy: str) -> None:
-            self.idn_kierowcy = idn_kierowcy
+    @idn_kierowcy.setter
+    def idn_kierowcy(self, idn_kierowcy: str) -> None:
+        self.idn_kierowcy = idn_kierowcy
 
     def __str__(self):
          return f'Miejscowość początkowa: {self.miejscowosc_poczatkowa}, ' \
