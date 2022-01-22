548e43e81f0f57d7340d630527df8473309022cf
Joanna <joannakrawczyk99@gmail.com>
2022-01-22 16:39:06 +0100

    initial

--- main.py
+++
@@ -1,7 +0,0 @@
-from Zamowienie import Zamowienie
-
-if __name__ == '__main__':
-    z = Zamowienie("123", "321", [1, 2, 3], [1000000, 12000000, 50000])
-    print(z)
-    print(z.sumuj_m2())
-    print(z.sumuj_cena())
